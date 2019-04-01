b96ee22b20bc05b3317cd6b2c5150f4e86c166b6
Menghua Zhai <zhaimenghua@matrixtime.com>
2019-04-01 19:16:27 +0800

    add training code

---
+++ samples/solar/solar_config.py
@@ -0,0 +1,50 @@
+#
+# Configuration class for solar
+#
+from mrcnn.config import Config
+
+SMALL_CONFIG=False   # test only when set it true
+
+class SolarConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "Solar"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 4  # background + 3 shapes
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 128 if SMALL_CONFIG else 448
+    IMAGE_MAX_DIM = 128 if SMALL_CONFIG else 448
+    
+    # Max number of final detections
+    DETECTION_MAX_INSTANCES = 10
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (16, 32, 64) if SMALL_CONFIG else (32, 64, 128, 256, 448)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 32
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 2 if SMALL_CONFIG else 1000
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 50
+    
+    # Interval (number of epochs) between checkpoints 
+    CHECKPOINT_EPOCH_INTERVAL = 100  # disable it and save the weights in the end
+    
+    
+    # faster rcnn mode
+    FASTER_RCNN_MODE = True
---
+++ samples/solar/train_solar.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import cv2
+
+import json
+import skimage.io, skimage.transform, skimage.color
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn.model import log
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.abspath("logs")
+
+
+#
+# Load configuration
+#
+from solar_config import SolarConfig, SMALL_CONFIG
+config = SolarConfig()
+config.display()
+
+
+#
+# Load training/testing data
+#
+from dataset_solar import get_dataset
+dataset_name = '20190401'
+
+# Training dataset
+dataset_train = get_dataset(dataset_name, 'train')
+dataset_train.prepare()
+
+# Validation dataset
+dataset_val = get_dataset(dataset_name, 'test')
+dataset_val.prepare()
+
+
+#
+# ## Create Model
+# Create model in training mode
+model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir=MODEL_DIR)
+
+
+#
+# Which weights to start with?
+#
+init_with = "coco"  # imagenet, coco, or last
+
+if init_with == "coco":
+    # Load weights trained on MS COCO, but skip layers that
+    # are different due to the different number of classes
+    # See README for instructions to download the COCO weights
+    # Local path to trained weights file
+    COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+    assert(os.path.exists(COCO_MODEL_PATH))
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])
+elif init_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last(), by_name=True)
+
+
+# ## Training
+# 
+# Train in two stages:
+# 1. Only the heads. Here we're freezing all the backbone layers and training only the randomly initialized layers (i.e. the ones that we didn't use pre-trained weights from MS COCO). To train only the head layers, pass `layers='heads'` to the `train()` function.
+# 
+# 2. Fine-tune all layers. For this simple example it's not necessary, but we're including it to show the process. Simply pass `layers="all` to train all layers.
+
+# define augmenter for training
+from imgaug import augmenters as iaa
+
+augmenters = iaa.Sequential([
+    iaa.Fliplr(.5),
+    iaa.Flipud(.5),
+    iaa.Crop((10,10), keep_size=True)  # jittering
+])
+
+
+# Train the head branches
+# Passing layers="heads" freezes all layers except the head
+# layers. You can also pass a regular expression to select
+# which layers to train by name pattern.
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE, 
+            augmentation=augmenters,
+            epochs=1 if SMALL_CONFIG else 200, 
+            layers='heads')
+
+
+# Save weights
+# Typically not needed because callbacks save after every epoch
+# Uncomment to save manually
+model_path = os.path.join(MODEL_DIR, "headonly.h5")
+model.keras_model.save_weights(model_path)
+
+
+# Fine tune all layers
+# Passing layers="all" trains all layers. You can also 
+# pass a regular expression to select which layers to
+# train by name pattern.
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE / 10,
+            augmentation=augmenters,
+            epochs=1 if SMALL_CONFIG else 200, 
+            layers="all")
+
+
+# Save weights
+# Typically not needed because callbacks save after every epoch
+# Uncomment to save manually
+model_path = os.path.join(MODEL_DIR, "complete.h5")
+model.keras_model.save_weights(model_path)
